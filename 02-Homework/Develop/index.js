d7bbc2c8cd3110733a27810ae98b757b08948720
CFM <cpierrelouis37@gmail.com>
2021-12-27 13:30:15 -0600

    Adding structure for the questions

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
-
+var generateREADME = require("./package.json")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "",
+        name: ""
+    },
+    {
+        type: "input",
+        message: "",
+        name: ""
+    },
+    {
+        type: "input",
+        message: "",
+        name: ""
+    },
+
+    {
+        type: "input",
+        message: "",
+        name: ""
+    },
+    {
+        type: "input",
+        message: "",
+        name: ""
+    }, {
+        type: "input",
+        message: "",
+        name: ""
+    }, {
+        type: "input",
+        message: "",
+        name: ""
+    }, {
+        type: "input",
+        message: "",
+        name: ""
+    }, {
+        type: "input",
+        message: "",
+        name: ""
+    }, {
+        type: "input",
+        message: "",
+        name: ""
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
